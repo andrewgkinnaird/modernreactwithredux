9eebf8bb7f25289c71f891df1d5aaa0526e52897
andrewgkinnaird <andy_kinnaird@yahoo.co.uk>
2022-04-24 12:54:12 +0000

    added conditions

--- my-app/seasons/src/index.js
+++ my-app/seasons/src/index.js
@@ -4,12 +4,39 @@ import ReactDOM from "react-dom";
 
 
 class App extends React.Component{
-    render(){
+
+    constructor(props){
+        super(props);
+        this.state = {lat:0,errorMessage:''};
+        
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (error) => console.log(error)
+            (position) => {
+                this.setState({lat:position.coords.latitude})
+            },
+            (error) => {
+                this.setState({errorMessage:error.message})
+            }
         );
-        return <div>Latitude: </div>
+    }
+    render(){
+        if(this.state.errorMessage && !this.state.lat)
+            return (
+                <div>
+                    Error: {this.state.errorMessage}
+                </div>
+            )
+        else if(!this.state.errorMessage && this.state.lat)      
+            return (
+                <div>
+                    Latitude: {this.state.lat}
+                </div>
+            )
+        else
+            return (
+                <div>
+                    Loading...
+                </div>
+            )
     }
 }
 
