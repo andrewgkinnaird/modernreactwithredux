eb26eeca2e4bba28c2722f7a1636e47e453c1888
andrewgkinnaird <andy_kinnaird@yahoo.co.uk>
2022-04-25 13:48:06 +0000

    added ternary exoressions

--- my-app/seasons/src/index.js
+++ my-app/seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from './SeasonDisplay';
 
 
 
@@ -28,7 +29,7 @@ class App extends React.Component{
         else if(!this.state.errorMessage && this.state.lat)      
             return (
                 <div>
-                    Latitude: {this.state.lat}
+                    <SeasonDisplay lat={this.state.lat}/>
                 </div>
             )
         else
