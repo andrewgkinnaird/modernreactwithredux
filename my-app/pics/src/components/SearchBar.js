5b877a1cd9b4bd86f3f18d298e7b169695dd8a67
andrewgkinnaird <andy_kinnaird@yahoo.co.uk>
2022-05-04 14:19:56 +0000

    added form functionality

--- my-app/pics/src/components/SearchBar.js
+++ my-app/pics/src/components/SearchBar.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 class SearchBar extends React.Component{
+    state = {
+        term:'Hi!'
+    };
+
+    formOnSubmit = (e) =>{
+        e.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.formOnSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="input"></input>
+                        <input type="input" value={this.state.term} onChange={e => this.setState({term:e.target.value.toUpperCase()})}></input>
                     </div>
                     
                 </form>
