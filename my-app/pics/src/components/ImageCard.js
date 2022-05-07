4372940b4275896b912c7b0144c92c646bc162b6
andrewgkinnaird <andy_kinnaird@yahoo.co.uk>
2022-05-07 05:07:02 +0000

    finished styling wuth grid using dynamic spans

--- my-app/pics/src/components/ImageCard.js
+++ my-app/pics/src/components/ImageCard.js
@@ -1,13 +1,30 @@
 import React from "react";
 
 class ImageCard extends React.Component{
-    
+    constructor(props){
+        super(props);
+        this.state = {span:0};
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount(){
+        this.imageRef.current.addEventListener('load',this.setSpan);
+        
+    }
+
+    setSpan = ()=>{
+        const height = this.imageRef.current.clientHeight;
+        const span = Math.ceil(height / 10 + 1);
+        this.setState({span});
+    }
+
     render(){
         const {urls, description} = this.props.image;
         return (
-            <div>
+            <div style={{gridRowEnd:`span ${this.state.span}`}}>
                 <img src={urls.regular}
                      alt={description}
+                     ref={this.imageRef}
                 />
             </div>
         )
