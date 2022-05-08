bc3bd8570dd532819745716d0c22e9da029ba79d
andrewgkinnaird <andy_kinnaird@yahoo.co.uk>
2022-05-08 13:10:26 +0000

    scaffolded searchbar

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 const App = () => {
     return (
         <div>
             Hi!
+            <SearchBar />
         </div>
     )
 }
