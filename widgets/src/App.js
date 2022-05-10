63418786da4228e641ef4b8b845349d303f359bf
andrewgkinnaird <andy_kinnaird@yahoo.co.uk>
2022-05-10 04:13:19 +0000

    added accordion component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title:'What is React?',
+        content:'Its a front end javascript framework.'
+    },
+    {
+        title:'Why use React?',
+        content:'It makes life easier.'
+    },
+    {
+        title:'Who uses React?',
+        content:'Many engineers use React.'
+    }
+]
 
 export default () => {
     return (
-        <h1>Widgets App</h1>
+        <div>
+            <Accordion items={items}/>
+        </div>
     )
 };
