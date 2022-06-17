90521a2527fd83b012134a8c79c8f2d4f49496b7
andrewgkinnaird <andy_kinnaird@yahoo.co.uk>
2022-06-17 14:03:45 +0000

    scaffolded app with all routes

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,8 +1,32 @@
 import React from "react";
+import {BrowserRouter, Route} from 'react-router-dom';
+import Header from "./Header";
+
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+
+
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route path='/' exact component={StreamList}/>
+                    <Route path='/streams/new' component={StreamCreate}/>
+                    <Route path='/streams/edit' component={StreamEdit}/>
+                    <Route path='/streams/delete' component={StreamDelete}/>
+                    <Route path='/streams/show' component={StreamShow}/>
+
+                </div>
+            </BrowserRouter>
+
+        </div>
     );
 }
 
