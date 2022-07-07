3ef7b668a192abbdea0379832bbebd786c5e534b
andrewgkinnaird <andy_kinnaird@yahoo.co.uk>
2022-07-07 03:20:00 +0000

    wired up sign in and sign out buttons for google

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 class GoogleAuth extends React.Component{
+    state = {isSignedIn:null};
+
     componentDidMount(){
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
@@ -8,11 +10,47 @@ class GoogleAuth extends React.Component{
                 scope:'email',
                 plugin_name:'Streamy'
             })
+            .then(()=>{
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn:this.auth.isSignedIn.get()});
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            })
         });
     }
+
+    onAuthChange = () => {
+        this.setState({isSignedIn:this.auth.isSignedIn.get()});
+    }
+
+    onSignIn = () => {
+        this.auth.signIn();
+    }
+
+    onSignOut = () => {
+        this.auth.signOut();
+    }
+
+    renderAuthButton(){
+        if(this.state.isSignedIn === null){
+            return null;
+        }
+        if(this.state.isSignedIn){
+            return <button onClick={this.onSignOut} className="ui red google button">
+                        <i className="google icon"/>
+                        Sign Out
+                    </button>
+        }
+        else if(!this.state.isSignedIn){
+            return <button onClick={this.onSignIn} className="ui red google button">
+                        <i className="google icon"/>
+                        Sign In
+                    </button>
+        }
+    }
+
     render(){
         return (
-            <div>Auth</div>
+            <div>{this.renderAuthButton()}</div>
         );
     }
 }
